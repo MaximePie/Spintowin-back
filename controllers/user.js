3726dd194c119057deab9818c428f4de9f962423
Chatmalow <maxime.pie.mail@gmail.com>
2022-04-10 12:19:41 +0200

    feat(Stats): Send successfully answered questions stats

--- controllers/user.js
+++ controllers/user.js
@@ -10,9 +10,9 @@ const mongoose = require("mongoose");
 const UserAnswer = require("../model/stats/userAnswer");
 
 const validationSchema = Joi.object({
-    username: Joi.string().min(6).required(),
-    email: Joi.string().min(6).required().email(),
-    password: Joi.string().min(6).required(),
+  username: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
 });
 
 /**
@@ -21,82 +21,82 @@ const validationSchema = Joi.object({
  * @param response
  */
 async function create(request, response) {
-    const {username, email, password, hasAdsEnabled} = request.body;
-    const userCredentials = {
-        username,
-        email,
-        password,
-    }
+  const {username, email, password, hasAdsEnabled} = request.body;
+  const userCredentials = {
+    username,
+    email,
+    password,
+  }
 
-    // Validation
-    const validation = validationSchema.validate(userCredentials);
-    if (validation.error) {
-        return response.json({
-            error: validation.error.details
-        })
-    }
+  // Validation
+  const validation = validationSchema.validate(userCredentials);
+  if (validation.error) {
+    return response.json({
+      error: validation.error.details
+    })
+  }
 
-    if (await emailExists(email)) {
-        return response.json({
-            message: "L'email est déjà utilisé",
-        })
-    }
+  if (await emailExists(email)) {
+    return response.json({
+      message: "L'email est déjà utilisé",
+    })
+  }
 
-    const hashedPassword = await bcrypt.hashSync(password, 8);
-    const user = new User({
-        username,
-        email,
-        password: hashedPassword,
-        hasAdsEnabled,
+  const hashedPassword = await bcrypt.hashSync(password, 8);
+  const user = new User({
+    username,
+    email,
+    password: hashedPassword,
+    hasAdsEnabled,
+  });
+  try {
+    user.save().then((user) => {
+      const token = jwt.sign({
+          _id: user._id,
+        },
+        process.env.TOKEN_SECRET
+      );
+      return response.json({
+        user: user._id,
+        token,
+      })
+    })
+  } catch (errors) {
+    response.json({
+      errors
     });
-    try {
-        user.save().then((user) => {
-            const token = jwt.sign({
-                    _id: user._id,
-                },
-                process.env.TOKEN_SECRET
-            );
-            return response.json({
-                user: user._id,
-                token,
-            })
-        })
-    } catch (errors) {
-        response.json({
-            errors
-        });
-    }
+  }
 }
 
 async function login(request, response) {
-    const {email, password} = request.body;
-    if (await emailExists(email)) {
-        const user = await User.findOne({email});
-        if (await isPasswordValid(password, user.password)) {
-            const token = jwt.sign({
-                    _id: user._id,
-                },
-                process.env.TOKEN_SECRET
-            );
+  const {email, password} = request.body;
+  if (await emailExists(email)) {
+    const user = await User.findOne({email});
+    if (await isPasswordValid(password, user.password)) {
+      const token = jwt.sign({
+          _id: user._id,
+        },
+        process.env.TOKEN_SECRET
+      );
 
-            response.header('auth-token', token).json({
-                token,
-            })
-        } else {
-            return response.json({
-                password: "Le mot de passe n'a pas été trouvé...",
-            })
-        }
+      response.header('auth-token', token).json({
+        token,
+      })
     } else {
-        return response.json({
-            message: "Email non trouvé.",
-        })
+      return response.json({
+        password: "Le mot de passe n'a pas été trouvé...",
+      })
     }
+  } else {
+    return response.json({
+      message: "Email non trouvé.",
+    })
+  }
 }
 
 module.exports.connectedUser = async function (request, response) {
-    const user = await User.findById(request.user._id);
-    return response.json(user);
+  const user = await User.findById(request.user._id);
+  return response.json(user);
 };
 
 /**
@@ -106,8 +106,8 @@ module.exports.connectedUser = async function (request, response) {
  * @returns A complete list of the users of the app
  */
 module.exports.index = async function (request, response) {
-    const users = await User.find({});
-    await response.json({users})
+  const users = await User.find({});
+  await response.json({users})
 }
 
 /**
@@ -118,9 +118,9 @@ module.exports.index = async function (request, response) {
  * @param response
  */
 module.exports.scales = async function (request, response) {
-    const user = await User.findById(request.user._id);
-    const results = await user.calculateMemorizedData();
-    return response.json(results);
+  const user = await User.findById(request.user._id);
+  const results = await user.calculateMemorizedData();
+  return response.json(results);
 };
 
 /**
@@ -131,31 +131,33 @@ module.exports.scales = async function (request, response) {
  * @returns {Promise<void>}
  */
 module.exports.answers = async function (request, response) {
-    const userId = mongoose.Types.ObjectId(request.user._id)
-    const wrongAnswers = await UserAnswer
-        .aggregate([
-            { $match : { userId, isSuccessful: false } },
-            {
-                $group:
-                    {
-                        _id: "$cardDelay",
-                        count: {$count: {}},
-                    }
-            },
-        ])
-    const answerDelays = await UserAnswer.aggregate(([
+  const userId = mongoose.Types.ObjectId(request.user._id)
+
+  const answerDelays = await UserAnswer.aggregate(([
+    {
+      $match: {userId}
+    },
+    {
+      $group:
         {
-            $match: {userId}
+          _id: "$cardDelay",
+          average: {$avg: "$answerDelay"},
+          succesfulAnswers: {$sum: {$cond: ["$isSuccessful", 1, 0]}},
+          wrongAnswers: {$sum: {$cond: ["$isSuccessful", 0, 1]}},
+          total: {$count: {}}
         },
-        {
-            $group:
-              {
-                  _id: "$cardDelay",
-                  average: { $avg: "$answerDelay"}
-              }
-        }
-    ]))
-    response.json({wrongAnswers, answerDelays});
+    },
+  ]))
+
+  const answersStats = answerDelays.map(({_id: delay, average: delayAverage, succesfulAnswers, wrongAnswers, total}) => {
+    return {
+      delay,
+      delayAverage,
+      successfulAnswersRate: Math.round(succesfulAnswers / total * 100)
+    }
+  })
+
+  response.json({answersStats});
 }
 
 
@@ -170,18 +172,18 @@ module.exports.answers = async function (request, response) {
  * @param response
  */
 module.exports.progress = async function (request, response) {
-    const user = await User.findById(request.user._id);
-    if (user) {
-        const results = await user.calculateProgressData();
-        return response.json(results);
-    }
-    return null;
+  const user = await User.findById(request.user._id);
+  if (user) {
+    const results = await user.calculateProgressData();
+    return response.json(results);
+  }
+  return null;
 };
 
 module.exports.badges = async function (request, response) {
-    const user = await User.findById(request.user);
-    const badges = await user.badges();
-    return response.json(badges)
+  const user = await User.findById(request.user);
+  const badges = await user.badges();
+  return response.json(badges)
 };
 
 /**
@@ -190,7 +192,7 @@ module.exports.badges = async function (request, response) {
  * @param testedPassword
  */
 function isPasswordValid(testedPassword, databasePassword) {
-    return bcrypt.compare(testedPassword, databasePassword);
+  return bcrypt.compare(testedPassword, databasePassword);
 }
 
 /**
@@ -198,9 +200,9 @@ function isPasswordValid(testedPassword, databasePassword) {
  * @param email
  */
 function emailExists(email) {
-    return User.findOne({
-        email
-    });
+  return User.findOne({
+    email
+  });
 }
 
 module.exports.create = create;
