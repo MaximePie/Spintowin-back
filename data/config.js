233f5580cca6f7185d10691e81960d6f7f782e49
MaximePie <maxime.pie.mail@gmail.com>
2023-06-08 07:20:26 +0200

    config: update displayed cards limit

--- data/config.js
+++ data/config.js
@@ -2,4 +2,5 @@ export function requiredExpForNextLevel(currentLevel) {
   return (500 * Math.pow(currentLevel, 2)) - (500 * currentLevel)
 }
 
-export const DISPLAYED_CARD_LIMIT = 50;
+// 50 is a weird and annoying behaviour of the app
+export const DISPLAYED_CARD_LIMIT = 49;
